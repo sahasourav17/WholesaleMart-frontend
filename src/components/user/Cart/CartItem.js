0764b2a2595736a6d3ba2dbcdcb96b8bb3e7eea7
sahasourav17 <sahasourav1170@gmail.com>
2024-06-05 00:53:23 +0600

    feat: navbar, cart style changes

--- src/components/user/Cart/CartItem.js
+++ src/components/user/Cart/CartItem.js
@@ -2,21 +2,16 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { removeCart } from "../../../features/cart/addToCartSlice";
+import createSlug from "../../../utils/createSlug";
 
-export const CartItem = ({ item }) => {
-  const { _id, name, picture, quantity, price } = item || {};
+export const CartItem = ({ product }) => {
   const dispatch = useDispatch();
-
-  //remove cart item handler
-  const removeCartItemHandler = (id) => {
-    dispatch(removeCart(id));
-  };
   return (
-    <div className="flex shadow-md p-1 mb-1">
-      <div className="h-20 w-20 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+    <li key={product._id} className="flex py-6">
+      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
         <img
-          src={picture}
-          alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
+          src={product.picture}
+          alt={product.imageAlt}
           className="h-full w-full object-cover object-center"
         />
       </div>
@@ -25,25 +20,32 @@ export const CartItem = ({ item }) => {
         <div>
           <div className="flex justify-between text-base font-medium text-gray-900">
             <h3>
-              <Link to="/">{name?.substring(0, 25)}</Link>
+              <Link
+                to={`/product-details/${createSlug(product.name)}/${
+                  product._id
+                }`}
+              >
+                {product.name}
+              </Link>
             </h3>
-            <p className="ml-4">${price}</p>
+            <p className="ml-4">{product.price}</p>
           </div>
-          {/* <p className="mt-1 text-sm text-gray-500">Salmon</p> */}
+          <p className="mt-1 text-sm text-gray-500">{product.color}</p>
         </div>
         <div className="flex flex-1 items-end justify-between text-sm">
-          <p className="text-gray-500">Qty {quantity}</p>
+          <p className="text-gray-500">Qty {product.quantity}</p>
 
-          <div className="flex" onClick={() => removeCartItemHandler(_id)}>
+          <div className="flex">
             <button
               type="button"
               className="font-medium text-indigo-600 hover:text-indigo-500"
+              onClick={() => dispatch(removeCart(product._id))}
             >
               Remove
             </button>
           </div>
         </div>
       </div>
-    </div>
+    </li>
   );
 };
